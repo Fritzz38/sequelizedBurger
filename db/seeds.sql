2464bc7400f7c376eb2ab0213e91d26660da118d
Tarmin Sidharta <tsidharta@gmail.com>
2017-08-03 10:10:29 -0700

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Grilled Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pepper Jack Burger');
+INSERT INTO burgers (burger_name) VALUES ('Lamb Burger');

134137bfda8ddadac82b855333a5c9b42cc95ba0
Tarmin Sidharta <tsidharta@gmail.com>
2017-08-07 00:52:14 -0700

    update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,24 +7,25 @@ var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-
-
-
+	db.Burger.findAll({}).then(function(results) {
+        res.render("index", { burgers: results });
+    });
 });
 
 
 router.post("/", function(req, res) {
-  
-
-
+	db.Burger.create({
+        "burger_name": req.body.burger_name
+    }).then(function() {
+        res.redirect("/");
+    });
 });
 
 
 router.put("/:id", function(req, res) {
-  
-
-
-  
+    db.Burger.update({ devoured: req.body.devoured }, { where: { id: req.params.id } }).then(function() {
+        res.redirect("/");
+    });  
 });
 
 
